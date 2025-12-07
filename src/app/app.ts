5d24f4ac64ef844cd70d605a80eaf349d7aa75b5
Brayan <02hxbrayan@gmail.com>
2025-12-07 13:41:18 -0500

    navbar con cerrar sesion funcional

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  private router = inject(Router);
+  mostrarNavbar = true;
   protected readonly title = signal('GestorNotas');
+  constructor() {
+    // Escuchar cambios en la ruta para mostrar u ocultar el navbar
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      const url = event.url;
+      // Ocultar navbar en las rutas de login y registro
+      this.mostrarNavbar = !url.includes('/login') && !url.includes('/registro');
+    });
+  }
 }
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-
+  private authService = inject(AuthService);
+  // obtener el email del usuario autenticado
+  get usuarioEmail(): string | null {
+    return this.authService.getUserEmail();
+  }
+  // cerrar sesión 
+  async cerrarSesion(): Promise<void> {
+    const confirmar = confirm('¿Estás seguro que deseas cerrar sesión?');
+    if (confirmar) {
+      await this.authService.logout();
+    }
+  }
 }
